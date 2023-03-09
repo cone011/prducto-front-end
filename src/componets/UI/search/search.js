b11a962bc2e1fa87817088fe90b8ca41b691ec76
Juan Jose Caimen Cespedes <32856@pycomunicaciones.local>
2023-03-09 12:09:24 -0300

    commit

--- src/componets/UI/search/search.js
+++ src/componets/UI/search/search.js
@@ -4,6 +4,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Search = (props) => {
   const { onReturnValue } = props;
+
+  return (
+    <div className={`${classes.containerSearch} mb-4`}>
+      <div className={classes.searchForm}>
+        <input
+          type="text"
+          name="search"
+          autoComplete="off"
+          placeholder="Search your product"
+        />
+        <button className={classes.btnClearInput} type="button">
+          <FontAwesomeIcon icon={faSearch} />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
---
+++ src/componets/products/productsForm/productsForm.js
@@ -0,0 +1,7 @@
+import classes from "./productsForm.module.css";
+
+const ProductItem = (props) => {
+  const { name, imgUrl, price, id } = props;
+};
+
+export default ProductItem;
