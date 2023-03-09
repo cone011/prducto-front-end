ab2228a3b47d0f375776db196584b12d5750c37d
Juan <jjcaimen@gmail.com>
2023-03-08 23:42:30 -0300

    commit

---
+++ src/componets/UI/search/search.js
@@ -0,0 +1,9 @@
+import classes from "./search.module.css";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Search = (props) => {
+  const { onReturnValue } = props;
+};
+
+export default Search;
